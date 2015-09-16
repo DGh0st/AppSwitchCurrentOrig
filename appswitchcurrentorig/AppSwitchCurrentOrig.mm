bebdc91c30332781f00b4ca8e2c1c2cb71809372
DGh0st <deeppwnage@yahoo.com>
2015-09-16 17:54:53 -0500

    Create AppSwitchCurrentOrig.mm

---
+++ appswitchcurrentorig/AppSwitchCurrentOrig.mm
@@ -0,0 +1,45 @@
+#import <Preferences/Preferences.h>
+#import <MessageUI/MFMailComposeViewController.h>
+
+@interface PSListController (AppSwitchCurrentOrig)
+- (void)presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion;
+- (void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion;
+- (UINavigationController*)navigationController;
+@end
+
+@interface AppSwitchCurrentOrigListController: PSListController <MFMailComposeViewControllerDelegate>{
+}
+@end
+
+@implementation AppSwitchCurrentOrigListController
+- (id)specifiers {
+	if(_specifiers == nil) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"AppSwitchCurrentOrig" target:self] retain];
+	}
+	return _specifiers;
+}
+-(void)email{
+	if([MFMailComposeViewController canSendMail]){
+		MFMailComposeViewController *email = [[MFMailComposeViewController alloc] initWithNibName:nil bundle:nil];
+		[email setSubject:@"AppSwitchCurrentOrig Support"];
+		[email setToRecipients:[NSArray arrayWithObjects:@"deeppwnage@yahoo.com", nil]];
+		[email addAttachmentData:[NSData dataWithContentsOfFile:@"/var/mobile/Library/Preferences/com.dgh0st.appswitchcurrentOrig.plist"] mimeType:@"application/xml" fileName:@"Prefs.plist"];
+		#pragma GCC diagnostic push
+		#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+		system("/usr/bin/dpkg -l >/tmp/dpkgl.log");
+		#pragma GCC diagnostic pop
+		[email addAttachmentData:[NSData dataWithContentsOfFile:@"/tmp/dpkgl.log"] mimeType:@"text/plain" fileName:@"dpkgl.txt"];
+		[self.navigationController presentViewController:email animated:YES completion:nil];
+		[email setMailComposeDelegate:self];
+		[email release];
+	}
+}
+- (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error {
+    [self dismissViewControllerAnimated: YES completion: nil];
+}
+- (void)donate{
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=deeppwnage%40yahoo%2ecom&lc=US&item_name=DGh0st&item_number=DGh0st%20Tweak%20Inc%20%28Wow%20I%20own%20a%20company%29&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHostedGuest"]];
+}
+@end
+
+// vim:ft=objc
